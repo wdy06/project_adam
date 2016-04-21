900833e5fd1bb143b956c47486c4e3e9958afd09
wada <mituhashitakashi@gmail.com>
2016-04-21 10:07:46 +0900

    bug fix

--- tools.py
+++ tools.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import csv
 
 def codeToFname(code):
-    return 'stock(' + str(code) + ').csv'
+    return 'stock(' + str(code) + ').CSV'
     
 def getClose(code):
     _close = []
+    filename = './stockdata/' + codeToFname(code)
     f = open(filename,'rb')
     reader = csv.reader(f)
     next(reader)
