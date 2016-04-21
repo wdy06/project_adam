7410e59fa888898c165832b66d3a38e44a31a606
wada <mituhashitakashi@gmail.com>
2016-04-21 10:25:46 +0900

    update getClose()

--- tools.py
+++ tools.py
@@ -5,14 +5,19 @@ import csv
 def codeToFname(code):
     return 'stock(' + str(code) + ').CSV'
     
-def getClose(code):
+def getClose(code, start_day):
+    _time = []
     _close = []
     filename = './stockdata/' + codeToFname(code)
     f = open(filename,'rb')
     reader = csv.reader(f)
     next(reader)
     for row in reader:
+        _time.append(float(row[0]))
         _close.append(float(row[4])*float(row[6]))
+        
     f.close()   
+    
+    _close = _close[_time.index(start_day):]
     
     return _close
