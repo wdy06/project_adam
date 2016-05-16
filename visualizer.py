08cec99951f79cd4f79ed4e6832a821cbe7b0175
wada <mituhashitakashi@gmail.com>
2016-05-16 17:35:42 +0900

    update

--- visualizer.py
+++ visualizer.py
@@ -24,7 +24,8 @@ parser = argparse.ArgumentParser(description='check result of model prediction')
 #parser.add_argument('checkfile', help='Path to check file name')
 parser.add_argument('code', help='stock code you check')
 parser.add_argument('model', help='Path to model')
-parser.add_argument('input_num', help='model input number',type=int)
+parser.add_argument('model_input_num', help='model input number',type=int)
+parser.add_argument('price_input_num', help='price input number',type=int)
 parser.add_argument('--tech','-t',default=None,
                     help='use tech or No')
 parser.add_argument('--param1', '-p1', default=None, type=int,
@@ -43,34 +44,33 @@ with open(args.model, 'rb') as m:
 
 outputlist = []
 predictlist = []
-
+error = []
 
 file = tools.codeToFname(args.code)
 
 if args.tech == None:
     print 'use price only'
-    train, test = md.getTeacherData(file,START_TEST_DAY,NEXT_DAY,args.input_num)
+    train, test = md.getTeacherData(file,START_TEST_DAY,NEXT_DAY,args.price_input_num)
 elif args.tech != None:
     print 'use price and technical indicator'
-    train, test = md.getTeacherDataTech(file,START_TEST_DAY,NEXT_DAY,args.input_num,args.tech,args.param1,args.param2,args.param3)
+    train, test = md.getTeacherDataTech(file,START_TEST_DAY,NEXT_DAY,args.price_input_num,args.tech,args.param1,args.param2,args.param3)
+
+
 
 for row in test:
-    inputlist = row[:args.input_num]
-    output = row[args.input_num]
+    inputlist = row[:args.model_input_num]
+    output = row[args.model_input_num]
     y = model.predict(np.array([inputlist]).astype(np.float32))
     outputlist.append(output)
     predictlist.append(y.data[0,0])
-    
+    error.append((output - y.data[0,0])*(output - y.data[0,0]))
+
 price = tools.getClose(args.code,START_TEST_DAY)
 #print len(test)
 #print len(outputlist), len(predictlist) ,len(price)
 
-
+tools.listToCsv(str(args.tech)+str(args.code)+'.csv',price,outputlist,predictlist,error)
 #可視化
-#plt.plot(outputlist)
-#plt.plot(predictlist)
-#plt.show()
-
 #2軸使用
 fig, axis1 = plt.subplots()
 axis2 = axis1.twinx()
@@ -78,8 +78,9 @@ axis1.set_ylabel('price')
 axis2.set_ylabel('move ratio')
 axis1.plot(price, label = "price")
 axis1.legend(loc = 'upper left')
-axis2.plot(output, label = 'True', color = 'g')
+axis2.plot(outputlist, label = 'True', color = 'g')
 axis2.plot(predictlist, label = 'predict', color = 'r')
+#axis2.plot(error, label = 'square error', color = 'm')
 axis2.legend()
 plt.show()
 print 'finished!!'
